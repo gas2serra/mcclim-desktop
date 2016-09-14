6185e5c905ed4e07edda10566f044ed7d5a5a6cd
Alessandro Serra <gas2serra@gmail.com>
2016-09-14 17:03:17 +0200

    add *default-server-path* option

--- dot-mcclim-desktop/config.lisp
+++ dot-mcclim-desktop/config.lisp
@@ -3,5 +3,17 @@
 (use-application-as-debugger "swank-debugger")
 (setf *application-style* :my)
 (find-applications)
+
+(ql:quickload :mcclim-clxv2/pretty)
+(ql:quickload :mcclim-clxv3/pretty)
+
 (configure-application (find-application :listener))
 (configure-application (find-application :climacs))
+
+
+(setf clim:*default-server-path*  (list :clx))
+#+nil
+(setf clim:*default-server-path*  (list :clxv3 :host ""
+					   :protocol :unix
+					   :display-id 0
+					   :mirroring :none))
