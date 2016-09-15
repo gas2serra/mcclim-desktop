66230b3d93e15236eaade81aa38417bccf278845
Alessandro Serra <gas2serra@gmail.com>
2016-09-15 23:17:59 +0200

    fix closure config

--- dot-mcclim-desktop/config/closure-config.lisp
+++ dot-mcclim-desktop/config/closure-config.lisp
@@ -3,7 +3,8 @@
 (setf (application-entry-fn *application*)
       #'(lambda (app &rest args)
 	  (declare (ignore app args))
-	  (closure:start)))
+	  (closure:start)
+	  (bt:join-thread clim-gui::*closure-process*)))
 
 (setf gui:*home-page* "http://www.cliki.net/")
 
