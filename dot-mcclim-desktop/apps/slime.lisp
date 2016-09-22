1158dd15e2cea894ba355244fc3de6239b76abd0
Alessandro Serra <gas2serra@gmail.com>
2016-09-22 20:26:36 +0200

    improve slime

--- dot-mcclim-desktop/apps/slime.lisp
+++ dot-mcclim-desktop/apps/slime.lisp
@@ -3,4 +3,5 @@
 (register-application "slime" 'standard-shell-application
 		      :pretty-name "Slime"
 		      :make-command-fn #'(lambda (&rest args)
-					   (format nil "emacs --eval '(slime-connect \"127.0.0.1\" 4005)'" args)))
+					   (declare (ignore args))
+					   (format nil "emacs --eval '(slime-connect \"127.0.0.1\" 4005)'")))
