41a7572167ae8194f9d80697bacd2dff10504b68
Alessandro Serra <gas2serra@gmail.com>
2017-01-15 22:55:31 +0100

    update config

--- dot-mcclim-desktop/config.lisp
+++ dot-mcclim-desktop/config.lisp
@@ -1,18 +1,9 @@
 (in-package :desktop-user)
 
-
 (setf *application-style* :my)
 (find-applications)
 
 (configure-application (find-application :listener))
 (configure-application (find-application :climacs))
 (use-application-as-debugger "panter-debugger")
-
-(setf clim:*default-server-path*  (list :clx))
-#+nil
-(progn
-  (ql:quickload :mcclim-clxv3/pretty)
-  (setf clim:*default-server-path*  (list :clxv3 :host ""
-					   :protocol :unix
-					   :display-id 0)))
 
