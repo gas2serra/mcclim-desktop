41a4182f3add0d4e12d050bc370f3db0d58a622a
Alessandro Serra <gas2serra@gmail.com>
2017-01-16 16:25:23 +0100

    improve debugger

--- Core/src/debugger.lisp
+++ Core/src/debugger.lisp
@@ -11,26 +11,47 @@
 ;;; debugger hook
 ;;;
 
+
+(defun desktop-swank-debugger-hook (condition me-or-my-encapsulation)
+  (unless swank::*connections*
+    (launch-app :swank-server)
+    (sleep 3.0)
+    (launch-app :slime)
+    (sleep 5.0))
+  (funcall #'swank:swank-debugger-hook condition me-or-my-encapsulation))
+
+(defun global-debugger-hook (condition me-or-my-encapsulation)
+  (format *debug-io* "GLOBAL DEB~%")
+  (swank/backend:call-with-debugger-hook
+   #'desktop-swank-debugger-hook
+   (lambda ()
+     (if (or (eq (climi::port-event-process (climi::find-port))
+		 (climi::current-process))
+	     (null *debugger*))
+	 (funcall  #'desktop-swank-debugger-hook condition me-or-my-encapsulation)
+	 (funcall *debugger* condition me-or-my-encapsulation)))))
+
 (defun debugger-hook (condition me-or-my-encapsulation)
-  (if (eq (climi::port-event-process (climi::find-port))
-	  (climi::current-process))
-      (funcall  #'swank:swank-debugger-hook condition me-or-my-encapsulation)
-      (when *debugger*
-	(funcall *debugger* condition me-or-my-encapsulation))))
+  (format *debug-io* "LOCAL DEB~%")
+  (swank/backend:call-with-debugger-hook
+   #'global-debugger-hook
+   (lambda ()
+     (if (or (eq (climi::port-event-process (climi::find-port))
+		 (climi::current-process))
+	     (null *debugger*))
+	 (funcall  #'desktop-swank-debugger-hook condition me-or-my-encapsulation)
+	 (funcall *debugger* condition me-or-my-encapsulation)))))
+
+;;; global variables
 
 (defvar *desktop-debugger-hook* #'debugger-hook)
+(defvar *desktop-global-debugger-hook* #'global-debugger-hook)
+
 ;;; functions
 
 (defun init-debugger ()
-  (setf *debugger* *debugger-hook*)
-  (setf *debugger-hook* #'debugger-hook))
+  (swank/backend:install-debugger-globally *desktop-global-debugger-hook*))
 
 (defun use-debugger (debugger)
   (log-info (format nil "Use debugger: ~A~%" debugger))
   (setq *debugger* debugger))
-
-;;; macros
-
-(defmacro with-debugger ((debugger) &body body)
-  `(let ((*debugger* ,debugger))
-     ,@body))
