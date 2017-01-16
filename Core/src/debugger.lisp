cabda12de996e72e959d7e453e147a8f2e574d31
Alessandro Serra <gas2serra@gmail.com>
2017-01-16 16:22:53 +0100

    improve debugger

--- Core/src/debugger.lisp
+++ Core/src/debugger.lisp
@@ -4,35 +4,54 @@
 ;;;; Debugging
 ;;;;
 
-#|
 (defvar *debugger* nil
   "The current debugger")
-
-;;; functions
-
-(defun use-debugger (debugger)
-  (setq *debugger* debugger))
-
-;;; macros
-
-(defmacro with-debugger ((debugger) &body body)
-  `(let ((*debugger* ,debugger))
-     ,@body))
 
 ;;;
 ;;; debugger hook
 ;;;
 
+
+(defun desktop-swank-debugger-hook (condition me-or-my-encapsulation)
+  (unless swank::*connections*
+    (launch-app :swank-server)
+    (sleep 3.0)
+    (launch-app :slime)
+    (sleep 5.0))
+  (funcall #'swank:swank-debugger-hook condition me-or-my-encapsulation))
+
+(defun global-debugger-hook (condition me-or-my-encapsulation)
+  (format *debug-io* "GLOBAL DEB~%")
+  (swank/backend:call-with-debugger-hook
+   #'desktop-swank-debugger-hook
+   (lambda ()
+     (if (or (eq (climi::port-event-process (climi::find-port))
+		 (climi::current-process))
+	     (null *debugger*))
+	 (funcall  #'desktop-swank-debugger-hook condition me-or-my-encapsulation)
+	 (funcall *debugger* condition me-or-my-encapsulation)))))
+
 (defun debugger-hook (condition me-or-my-encapsulation)
-  (when *debugger*
-    (funcall *debugger* condition me-or-my-encapsulation)))
-|#
+  (format *debug-io* "LOCAL DEB~%")
+  (swank/backend:call-with-debugger-hook
+   #'global-debugger-hook
+   (lambda ()
+     (if (or (eq (climi::port-event-process (climi::find-port))
+		 (climi::current-process))
+	     (null *debugger*))
+	 (funcall  #'desktop-swank-debugger-hook condition me-or-my-encapsulation)
+	 (funcall *debugger* condition me-or-my-encapsulation)))))
 
-(defvar *desktop-debugger-hook* nil)
-(defgeneric use-debugger (debugger))
+;;; global variables
 
-(defmethod use-debugger (debugger)
+(defvar *desktop-debugger-hook* #'debugger-hook)
+(defvar *desktop-global-debugger-hook* #'global-debugger-hook)
+
+;;; functions
+
+(defun init-debugger ()
+  (swank/backend:install-debugger-globally *desktop-global-debugger-hook*))
+
+(defun use-debugger (debugger)
   (log-info (format nil "Use debugger: ~A~%" debugger))
-  (setf *desktop-debugger-hook* debugger)
-  (setf *debugger-hook* debugger))
-
+  (setq *debugger* debugger))
